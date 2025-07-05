c8419e4d8eca2463bc08e6f33bc51a3f62872e71
alexmorin7772 <alexmorin7772@gmail.com>
2025-07-05 12:01:24 -0400

    combined huskyRead thread

--- R2D2/R2D2.ino
+++ R2D2/R2D2.ino
@@ -1,11 +1,14 @@
 #include "protothreads.h"
 
 pt ptEyelid;
+pt ptHuskylens;
 
 void setup() {
   PT_INIT(&ptEyelid);
+  PT_INIT(&ptHuskylens);
 }
 
 void loop() {
   PT_SCHEDULE(eyelidThread(&ptEyelid));
+  PT_SCHEDULE(huskyRead(&ptHuskylens));
 }
---
+++ R2D2/Vision.ino
@@ -0,0 +1,38 @@
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+#include "protothreads.h"
+
+HUSKYLENS huskylens;
+SoftwareSerial mySerial(2, 3); // RX, TX
+//HUSKYLENS green line >> Pin 2; blue line >> Pin 3
+void printResult(HUSKYLENSResult result);
+
+int huskyRead(struct pt* husky) {
+  PT_BEGIN(husky);
+  for(;;) {
+    if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+      //else if(!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+      //else if(!huskylens.available()) Serial.println(F("No block or arrow appears on the screen!"));
+      else {
+       //Serial.println(F("###########"));
+        while (huskylens.available()) {
+          HUSKYLENSResult result = huskylens.read();
+          printResult(result);
+        }
+      }
+      PT_SLEEP(husky, 1);
+    }
+  PT_END(husky);
+}
+
+void printResult(HUSKYLENSResult result){
+    if (result.command == COMMAND_RETURN_BLOCK){
+        Serial.println(String()+F("Block:xCenter=")+result.xCenter+F(",yCenter=")+result.yCenter+F(",width=")+result.width+F(",height=")+result.height+F(",ID=")+result.ID);
+    }
+    else if (result.command == COMMAND_RETURN_ARROW){
+        Serial.println(String()+F("Arrow:xOrigin=")+result.xOrigin+F(",yOrigin=")+result.yOrigin+F(",xTarget=")+result.xTarget+F(",yTarget=")+result.yTarget+F(",ID=")+result.ID);
+    }
+    else{
+        Serial.println("Object unknown!");
+    }
+}
