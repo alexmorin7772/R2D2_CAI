faac76cf4973a43dbde42fbb608f220e1537a66d
fleomorin <fleomorin@gmail.com>
2025-07-05 12:28:22 -0400

    added servo code example

--- R2D2/R2D2.ino
+++ R2D2/R2D2.ino
@@ -7,7 +7,7 @@ pt ptHuskylens;
 Servo myservo;  // create Servo object to control a servo
 // twelve Servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;  // variable to store the servo position
 
 void setup() {
   PT_INIT(&ptEyelid);
