9148147fc062be8ee1a2776d3a96fc64f88b31a5
fleomorin <fleomorin@gmail.com>
2025-07-05 12:22:36 -0400

    adding template files

--- R2D2/Eyelid.ino
+++ R2D2/Eyelid.ino
@@ -1,7 +1,15 @@
 int eyelidThread(struct pt* pt) {
   PT_BEGIN(pt);
   for(;;) {
-    // PT_SLEEP(pt, #);
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      PT_SLEEP(pt, 15);                       // waits 15 ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      PT_SLEEP(pt, 15);                       // waits 15 ms for the servo to reach the position
+    }
   }
   PT_END(pt);
 }
--- R2D2/R2D2.ino
+++ R2D2/R2D2.ino
@@ -1,11 +1,18 @@
 #include "protothreads.h"
+#include <Servo.h>
 
 pt ptEyelid;
 pt ptHuskylens;
 
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
   PT_INIT(&ptEyelid);
   PT_INIT(&ptHuskylens);
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
 }
 
 void loop() {
