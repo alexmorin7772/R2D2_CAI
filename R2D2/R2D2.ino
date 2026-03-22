c3a384c751e2cd7d760b60cde3ecd9cb09f1505d
Rip_Savage29 <130927902+Ajsrivat29@users.noreply.github.com>
2026-03-22 15:54:10 -0400

    Update R2D2.ino

--- R2D2/R2D2.ino
+++ R2D2/R2D2.ino
@@ -1,19 +1,143 @@
 #include "protothreads.h"
+#include "SoftwareSerial.h"
+#include "HUSKYLENS.h"
+#include <Wire.h>
+#include <Digital_Light_TSL2561.h>
 #include <Servo.h>
 
+/*
+The following are all defined constants
+#define HIGH 0x1
+#define LOW  0x0
+
+#define INPUT 0x0
+#define OUTPUT 0x1
+#define INPUT_PULLUP 0x2
+
+#define PI 3.1415926535897932384626433832795
+#define HALF_PI 1.5707963267948966192313216916398
+#define TWO_PI 6.283185307179586476925286766559
+#define DEG_TO_RAD 0.017453292519943295769236907684886
+#define RAD_TO_DEG 57.295779513082320876798154814105
+#define EULER 2.718281828459045235360287471352
+
+#define SERIAL  0x0
+#define DISPLAY 0x1
+
+#define LSBFIRST 0
+#define MSBFIRST 1
+
+#define CHANGE 1
+#define FALLING 2
+#define RISING 3
+*/
+
+//Create all protothread structs
 pt ptEyelid;
 pt ptHuskylens;
+pt pt_ball_distance;
+pt pt_goal_distance;
+pt pt_lens_adjustment;
+
+//Huskylens variables
+HUSKYLENS huskylens;
+SoftwareSerial serial(10, 11);
+
+struct location {
+  int x, y;
+};
+
+//ball distance variables
+float ball_size_10 = 120.0;
+float ball_current_distance;
+float ball_current_size;
+bool ball_success;
+location ball_location;
+
+//goal distance variables
+float goal_size_30 = 100.0;
+float goal_current_distance;
+float goal_height;
+bool goal_success;
+location goal_location;
+
+const double BALL_DIAMETER = 4.27;
+//ball diameter in cm
+
+typedef enum object_recognition_state {
+  start,
+  initialization,
+  test_for_object,
+  check_time,
+  evaluate_distance,
+  finish
+};
+
+//do NOT write to any of these variables except for is_most_recent
+typedef struct object_recognition_results {
+  bool is_most_recent = false; //this will be changed to true once the Huskylens writes to it
+  //once someone reads it, it should be set to false so the same information isn't used again
+  float object_distance;
+  float object_size;
+  location object_location;
+  //stores the x and y coordinates of the ball (do object_location.x or object_location.y to get the x and y values)
+};
+
+//Initialize functions
+int ball_distance(struct pt* pt);
+int eyelidThread(struct pt* pt);
+int goal_distance(struct pt* pt);
+
+//state machine variables
+int servo_position = 0;
+
+//other variables
+unsigned long max_milliseconds = 400; //0.4 second
 
 Servo myservo;  // create Servo object to control a servo
-// twelve Servo objects can be created on most boards
 
 int pos = 0;  // variable to store the servo position
 
+typedef enum lens_adjustment_state {
+  lens_start,
+  read_lux,
+  calculate_angle,
+  move_servo,
+  lens_finish
+};
+
+int lux = 9999;
+int actual_lux = 9999;
+int goal_lux = 500;
+int angle = 0;
+
+const int OUT1 = -1;
+const int OUT2 = -1;
+//the OUT1 and OUT2 pins go to analog pins and we read them to figure out whether to start or stop
+//remember to change the OUT1 and OUT2 pins when we decide on it
+
+bool started = false;
+//boolean for the start/stop signal
+
+const float ANALOG_RANGE = 3.3;
+const float ANALOG_MAX = 1023.0;
+//range of analog voltages and the maximum possible analog reading
+const float ON = 3.3;
+const float OFF = 0.0;
+//voltage for on and off signals
+
 void setup() {
   Serial.begin(115200);
+  serial.begin(9600);
+  Wire.begin();
+  TSL2561.init();
   PT_INIT(&ptEyelid);
   PT_INIT(&ptHuskylens);
+  PT_INIT(&pt_ball_distance);
+  PT_INIT(&pt_goal_distance);
+  PT_INIT(&pt_lens_adjustment);
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  while (!huskylens.begin(serial)) Serial.println("Begin failed!");
   // huskyAlgorithm();   // Huskylens - Vision.ino
   // motorSetup();       // Motor Driver - 
   // groveDLSsetup();    // Light Sensor - 
@@ -22,13 +146,12 @@ void setup() {
 }
 
 void loop() {
-  PT_SCHEDULE(eyelidThread(&ptEyelid));
-  PT_SCHEDULE(huskyRead(&ptHuskylens));
+  //PT_SCHEDULE(eyelidThread(&ptEyelid));
+  //PT_SCHEDULE(huskyRead(&ptHuskylens));
+  //PT_SCHEDULE(ball_distance(&pt_ball_distance));
+  //PT_SCHEDULE(goal_distance(&pt_goal_distance));
+  PT_SCHEDULE(lens_adjustment(&pt_lens_adjustment, goal_lux));
 }
-
-
-
-
 
 
 const int MOTOR_LEFT_FWD  = 3;
